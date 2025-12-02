4cc3a7809c686f7b24f08f0258ffc89c7e0e7f06
Viet <quoc.viet2790@gmail.com>
2025-12-02 19:09:48 +0700

    feat: add solution for challenge 04

---
+++ tests/lesson-04/moon-kingdom.js
@@ -0,0 +1,33 @@
+// Exercise 1
+
+const characters = [
+    { name: "Viet", level: 1, health: 200 },
+    { name: "Phong", level: 2, health: 200 },
+    { name: "Nam", level: 5, health: 1000 },
+    { name: "Hoai", level: 8, health: 500 },
+]
+const newCharacters = characters.map(character => ({
+    name: character.name.toUpperCase(),
+    level: character.level * 2,
+    health: character.health * 3,
+}));
+let possibleWinners = newCharacters.filter(characters => characters.health > 1000);
+console.log(possibleWinners);
+
+// Exercise 2
+
+const players = [
+    { name: "Mario", score: 1000 },
+    { name: "Luigi", score: 950 },
+    { name: "Peach", score: 850 },
+    { name: "Yoshi", score: 1500 },
+    { name: "Viet", score: 500 }
+]
+const medals = ["🥇 Gold", "🥈 Silver", "🥉 Bronze"];
+players.sort((a, b) => b.score - a.score);
+players.forEach((player, index) => {
+    if (index < medals.length) {
+        player.medals = medals[index];
+    }
+});
+console.log(players);
