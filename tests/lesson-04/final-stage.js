4e42bc4e955cd102850e5b45c40c719a8b36f9f5
Viet <quoc.viet2790@gmail.com>
2025-12-04 21:37:13 +0700

    feat: add solution for final stage

--- tests/lesson-04/final-stage.js
+++ tests/lesson-04/final-stage.js
@@ -1,6 +1,6 @@
 ﻿function findPairsDivisibleBy17() {
     const pairs = [];
-    for (x = 1; x < 100; x++) {
+    for (x = 0; x < 100; x++) {
         for (y = x++; y <= 100; y++) {
             var sum = x + y;
             if (sum % 17 === 0) {
