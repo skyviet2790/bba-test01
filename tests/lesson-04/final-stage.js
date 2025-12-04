00f73752954efd907f312234845a70962575ec27
Viet <quoc.viet2790@gmail.com>
2025-12-04 21:08:05 +0700

    feat: add solution for final stage

---
+++ tests/lesson-04/final-stage.js
@@ -0,0 +1,17 @@
+﻿function findPairsDivisibleBy17(){
+    const pairs = [];
+    for (x = 1; x < 100; x++){
+        for (y = x++; y <= 100; y++){
+            var sum = x + y;
+            if (sum % 17 ===0){
+                pairs.push([`${x}`, `${y}`]);
+            }
+        }
+    }
+    pairs.forEach((pair) => {
+        console.log(pair);
+    });
+}
+
+findPairsDivisibleBy17();
+
